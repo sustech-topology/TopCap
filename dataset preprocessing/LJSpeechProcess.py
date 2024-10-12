2deef1a66d8aa7df0fd28345891717983505d215
QingruiQu <116701258+QingruiQu@users.noreply.github.com>
2024-10-12 14:31:07 +0800

    Update LJSpeechProcess.py

--- dataset preprocessing/LJSpeechProcess.py
+++ dataset preprocessing/LJSpeechProcess.py
@@ -11,22 +11,23 @@ import csv
 import re
 import os
 
-# 打开原始CSV文件和目标CSV文件
+# 打开原始CSV文件和目标CSV文件Open the original CSV file and the target CSV file
 with open('D:\\phonetic\\test_csv\\metadata.csv', 'r',encoding='utf-8') as input_file, open('correctedmetadata.csv', 'w', newline='',encoding='utf-8') as output_file:
     reader = csv.reader(input_file)
     writer = csv.writer(output_file)
 
-    # 遍历原始CSV文件的每一行
+    # 遍历原始CSV文件的每一行Traverse each row of the original CSV file
     for row in reader:
-        # 将一行数据合并为字符串
+        # 将一行数据合并为字符串Combine a row of data into a string
         row_string = ' '.join(row)
 
         #使用 split() 函数和正则表达式方法将字符串按'LJ0'分割为多个子串，并通过列表推导式筛选出以 "LJ0" 开头的子串
+        #Use the split() function and a regular expression method to split the string by 'LJ0' into multiple substrings, and filter the substrings that start with 'LJ0' using a list comprehension.
         matches =  re.split(r"(?=LJ0)", row_string)
         matches = [substring for substring in matches if substring.startswith("LJ0")]
 
 
-        # 写入目标CSV文件的多行
+        # 写入目标CSV文件的多行Write multiple rows to the target CSV file
         for match in matches:
             writer.writerow([match])
             
@@ -37,24 +38,25 @@ with open('D:\\phonetic\\test_csv\\metadata.csv', 'r',encoding='utf-8') as input
 csv_file = 'D:\\phonetic\\test_csv\\correctedmetadata.csv'  # CSV 文件的路径
 output_folder = 'textgrid_files'  # 存放生成的 TextGrid 文件的文件夹
 
-# 创建存放 TextGrid 文件的文件夹
+# 创建存放 TextGrid 文件的文件夹Create a folder to store the TextGrid files
 os.makedirs(output_folder, exist_ok=True)
 
-# 打开 CSV 文件
+# 打开 CSV 文件 Open CSV file
 with open(csv_file, 'r',encoding='utf-8') as file:
     reader = csv.reader(file)
     
-    # 逐行读取 CSV 文件
+    # 逐行读取 CSV 文件Read the CSV file line by line
     for row in reader:
         if len(row) > 0:
-            line = row[0]  # 获取每行的字符串
-            filename = line[:10]  # 使用前10个字符作为文件名
-            content = line[11:]  # 去除前11个字符的内容
+            line = row[0]  # 获取每行的字符串Obtain the string from each line.
+            filename = line[:10]  # 使用前10个字符作为文件名Use the first 10 characters as the filename
+            content = line[11:]  # 去除前11个字符的内容Remove the first 11 characters from the content
             content = content[len(content)//2+1:] #除以2是因为文本有两份，后面的数字写成单词形式，取后一半方便MFA识别
-            # 创建 TextGrid 文件的路径
+            # Divide by 2 because the text is duplicated. Convert the subsequent numbers into word form and take the latter half for easier MFA recognition. 
+            # 创建 TextGrid 文件的路径create the path for the TextGrid file
             file_path = os.path.join(output_folder, f'{filename}.TextGrid')
             
-            # 将字符串写入 TextGrid 文件                
+            # 将字符串写入 TextGrid 文件 Write the string to the TextGrid file.                
             with open(file_path, 'w',encoding='utf-8') as textgrid_file:
                 textgrid_file.write('File type = "ooTextFile"\n')
                 textgrid_file.write('Object class = "TextGrid"\n')
